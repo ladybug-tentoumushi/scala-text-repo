97b8ff7155da93fa1d5c467e8ba6f3c521c0e3a4
ladybug <ladybug.tentoumushi@gmail.com>
2023-03-18 20:25:59 +0900

    ScalaテキストFutureとPromiseのFuture例文を写経

---
+++ src/main/scala/CompositeFutureSample.scala
@@ -0,0 +1,26 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Random}
+
+object FutureSample3 {
+  val random = new Random()       //randomという名前の変数を定義。scala.util.Randomオブジェクト
+  val waitMaxMillSec = 3000       //waitMaxという名前の変数を定義。待ち時間の上限を表す整数
+
+  def main(args: Array[String]): Unit = {                     //mainメソッドを定義
+    def waitRandom(futureName: String): Int ={                //waitRandom関数を定義
+      val waitMillSec = random.nextInt(waitMaxMillSec)        //nextInt()メソッドは、0から指定された引数(waitMaxMilliSec)の間のランダムな整数を生成します。つまり、waitMaxMilliSecが3000である場合、nextInt()メソッドは0から2999までの整数を生成します。その後、このランダムな整数値が、変数waitMilliSecに割り当てられ、この値が後で使用されて、ランダムな待機時間を表します。
+      if(waitMillSec < 500) throw new RuntimeException(s"${futureName} waitMillSec is ${waitMillSec}")  //ランダムな待機時間が500msec未満の場合、RuntimeExceptionをスロー　
+      Thread.sleep(waitMillSec)
+      waitMillSec
+    }
+
+    val futureFirst: Future[Int] = Future{waitRandom("first")}
+    val futureSecond: Future[Int] = Future{ waitRandom("seconf")}
+
+    val compositeFuture: Future[(Int, Int)] = for {
+      first <- futureFirst
+      second <- futureSecond
+    } yield()
+  }
+
+}
---
+++ src/main/scala/FutureSample1.scala
@@ -0,0 +1,40 @@
+import scala.concurrent.Future                            //ScalaのFutureライブラリ（非同期処理を実行できるライブラリ）を呼び出す*
+import scala.concurrent.ExecutionContext.Implicits.global //並行処理するためのスレッド（メモリ？）を用意するライブラリ。普通の並列処理をしたいならこれでいい（大量の処理をしたいときは別）*
+//*Futureを作成する（非同期処理を実行する）にはconcurrent.Futureをインポートする必要がある。
+//Futureを使用するにはExecutionContextをインポートする必要がある。
+
+object FutureSample {
+
+  def main(args: Array[String]): Unit = {
+    val s = "Hello"                                       //変数sにHelloを代入
+    val f: Future[String] = Future {                      //Future（非同期処理を実行できる型）を作成（標準ライブラリのFuture.applyメソッドまたはFuture.successful/Future.failedメソッドを使用して作成できる。）。変数fに代入
+      Thread.sleep(1000)                                  //スレッドを指定された時間（1000msec）停止する（タイムアウト処理なんかに使われる）
+      s + "future!"                                       //文字列s + future!を返す（メソッドの最後に書かれた処理が戻り値になる（Futureの戻り値＝文字列s + future!））
+    }
+
+    f.foreach { s =>                                      //f(Future)が終了したら呼び出されるメソッド（コールバック関数）
+      println(s)                                          //処理の結果が返って来るだけなので、defやvalは必要ない
+    }
+
+    println(f.isCompleted)                                //isCompletedメソッドを使って、Futureの完了状態を出力させる→まだ完了してないのでfolse
+
+    Thread.sleep(5000)                                     //５秒後にFutureが完了してforeachで定義した処理が実行（Hello future!が出力）
+
+    println(f.isCompleted)                                  //もう一度isCompletedメソッドを使って、Futureの完了状態を出力させる
+
+
+    val f2: Future[String] = Future {                       //f2という新しいFutureを作成
+      Thread.sleep(1000)//1000ms後に↓の処理を実施
+      throw new RuntimeException("わざと失敗")//例外処理でエラーをださせる
+    }
+
+    f2.failed.foreach(e =>                                  //Futureの処理が失敗した場合の処理
+    println(e.getMessage))
+
+    println(f2.isCompleted)
+
+    Thread.sleep(5000)
+
+    println(f2.isCompleted)
+  }
+}
