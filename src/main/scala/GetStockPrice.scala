75a79f0153520ddff118bd2cffa3c365b3e734fa
ladybug <ladybug.tentoumushi@gmail.com>
2023-04-11 02:22:32 +0900

    株価を取得できなかったパターンを追加

--- src/main/scala/GetStockPrice.scala
+++ src/main/scala/GetStockPrice.scala
@@ -1,3 +1,24 @@
-object GetStockPrice {
+import scala.io.Source
+import scala.util.{Try,Success, Failure}
 
+object GetStockPrice {
+  def main(args: Array[String]): Unit = {
+    val tickerSymbol = "花王"
+    val targetPrice = 5000
+    val url = s"https://finance.yahoo.co.jp/search/?query=%E8%8A%B1%E7%8E%8B"
+
+    Try(Source.fromURL(url))match {
+      case Success(response) =>
+        val priceRegex = """<span class="_1-yujUee"><span class="_3rXWJKZF">([\d,]+\d+)""".r
+        val price = priceRegex.findFirstMatchIn(response.mkString).map(_.group(1).replaceAll(",", "").toInt)
+        price match {
+          case Some(price) if price < targetPrice =>
+            println(s"$tickerSymbol の株価が $targetPrice 以下になりました。")
+          case Some(price) =>  println(s"$tickerSymbol は $price です")
+          case _ =>  println(s"$tickerSymbol の株価を取得できませんでした。")
+        }
+      case Failure(exception) =>
+        println(s"エラー: $exception")
+    }
+  }
 }
