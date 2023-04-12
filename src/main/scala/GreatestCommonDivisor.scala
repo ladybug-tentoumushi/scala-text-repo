a88ad1a637e32d128ac5e9866de06e41ca9527f8
ladybug <ladybug.tentoumushi@gmail.com>
2023-04-12 14:08:50 +0900

    mapを使ってxとyを一気にBigIntにできるようにコードを修正しました

--- src/main/scala/GreatestCommonDivisor.scala
+++ src/main/scala/GreatestCommonDivisor.scala
@@ -3,9 +3,7 @@ import scala.io.StdIn.readLine
 
 object GreatestCommonDivisor extends App {
   val imput = readLine()
-  val Array(imputX, imputY) = imput.split(" ")
-  val x = BigInt(imputX)
-  val y = BigInt(imputY)
+  val Array(x, y) = imput.split(" ").map(BigInt(_))
   val result = x.gcd(y)
   println(result)
 }
