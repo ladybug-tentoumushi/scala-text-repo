00c0c3e68a6a3cc931d67f219e7acc333ace64de
ladybug <ladybug.tentoumushi@gmail.com>
2023-04-12 10:30:49 +0900

    forを使ってナイーブ法のアルゴリズムで文字列検索できるようにコードを修正しました

--- src/main/scala/NativeStrSearch.scala
+++ src/main/scala/NativeStrSearch.scala
@@ -1,24 +1,20 @@
 //ALDS1_14_A
-//import scala.io.StdIn.readLine
+import scala.io.StdIn.readLine
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      println("Usage: NativeStrSearch <T> <P>")
-      System.exit(1)
-    }
-    val t: String = args(0)
-    val p: String = args(1)
-    nativeStrSearch(t, p)
-  }
+object Main extends App {
+    val t = readLine()
+    val p = readLine()
+    val matchIndexes = println(search(t, p))
 
-  def nativeStrSearch(t: String, p: String): Unit = {
-    val tLength = t.length
-    val pLength = p.length
-    val result: List[Int] = (0 to tLength - pLength).flatMap {
-      case i if t.substring(i, i + pLength) == p => Some(i)
-      case _ => None
-    }.toList
-    result.foreach(println)
+  def search(t:String, p:String): Seq[Int] = {
+    var matchIndexes = Seq[Int]()
+
+    for (i <- 0 to t.length - p.length) {
+      val partText = t.slice(i, i + p.length)
+      if (partText == p) {
+        matchIndexes :+= i
+      }
+      }
+     matchIndexes
   }
 }
