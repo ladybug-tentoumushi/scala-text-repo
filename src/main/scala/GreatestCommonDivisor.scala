6dd547c65e89b1ca5ca315cd29d3c32456d82526
ladybug <ladybug.tentoumushi@gmail.com>
2023-04-12 14:04:46 +0900

    AOJ ALDS1_1_B 最大公約数を求めるコードです

--- src/main/scala/GreatestCommonDivisor.scala
+++ src/main/scala/GreatestCommonDivisor.scala
@@ -1,3 +1,11 @@
-object GreatestCommonDivisor {
+//ALDS1_1_B
+import scala.io.StdIn.readLine
 
+object GreatestCommonDivisor extends App {
+  val imput = readLine()
+  val Array(imputX, imputY) = imput.split(" ")
+  val x = BigInt(imputX)
+  val y = BigInt(imputY)
+  val result = x.gcd(y)
+  println(result)
 }
