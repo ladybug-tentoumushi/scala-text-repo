a3f48924dbaaa4b43d9627586a35c9e87bda17d1
ladybug <ladybug.tentoumushi@gmail.com>
2023-04-14 13:35:59 +0900

    要素を交換した数がカウントできるようにforを使ったループ構文に修正しました

--- src/main/scala/AOJBubbleSort.scala
+++ src/main/scala/AOJBubbleSort.scala
@@ -1,7 +1,34 @@
 //ALDS1_2_A
 import scala.io.StdIn.readLine
 
-object AOJBubbleSort extends App {
+object Main extends App {
+  val n: Int = readLine().toInt
+  val nums: Array[Int] = readLine.split(" ").map(_.toInt)
+
+
+  def bubbleSort(nums: Array[Int], n: Int): (Array[Int], Int) = {
+    var count = 0
+    if (n <= 1) (nums, count)
+    else {
+      for (i <- 0 until n - 1) {
+        if (nums(i) > nums(i + 1)) {
+          val temp = nums(i)
+          nums(i) = nums(i + 1)
+          nums(i + 1) = temp
+          count += 1
+        }
+      }
+      val result = bubbleSort(nums: Array[Int], n - 1)
+      (result._1, count + result._2)
+    }
+  }
+
+  val (resultnums, swaps) = bubbleSort(nums, n)
+  println(resultnums.mkString(" "))
+  println(swaps)
+}
+
+/*object AOJBubbleSort extends App {
   val n:Int = readLine().toInt
   val nums: List[Int] = readLine.split(" ").toList.map(_.toInt)
   val result = bubbleSort(nums)
@@ -14,5 +41,5 @@ object AOJBubbleSort extends App {
       if (x > y) y :: bubbleSort(x :: rest)
       else x :: bubbleSort(y :: rest)
   }
-
 }
+*/
