cf0387d94fc484dd14cf71b76a4801a346f5c5a6
ladybug <ladybug.tentoumushi@gmail.com>
2023-04-14 14:25:23 +0900

    AOJ ALDS1_3_A 逆ポーランド記法の数式を計算するコードを作成しました

--- src/main/scala/AOJStack.scala
+++ src/main/scala/AOJStack.scala
@@ -1,3 +1,32 @@
-object AOJStack {
+//ALDS1_3_A
+
+import scala.io.StdIn.readLine
+import scala.collection.mutable.Stack
+
+object AOJStack extends App {
+  val inputs:Array[String] = readLine.split(" ")
+  val result = stack(inputs)
+  println(result)
+
+  def stack(inputs: Array[String]): Int = {
+    val stack = new Stack[Int]
+
+    for (x <- inputs) {
+      if (x == "+" || x == "-" || x == "*" || x == "/") {
+        val right = stack.pop()
+        val left = stack.pop()
+        x match {
+          case "+" => stack.push(left + right)
+          case "-" => stack.push(left - right)
+          case "*" => stack.push(left * right)
+          case "/" => stack.push(left / right)
+        }
+      } else {
+        stack.push(x.toInt)
+      }
+    }
+
+    stack.top
+  }
 
 }
